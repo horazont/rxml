8f72ec99487d79640acbc7c6ce212bb1ecf6a1f1
Jonas Schäfer <j.wielicki@sotecware.net>
2021-05-31 21:30:35 +0200

    fuzz: exhaustively try chunking positions
    This is terribly slow and we’ll look for another way.

--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -5,18 +5,48 @@ extern crate rxml;
 use std::io;
 use rxml::EventRead;
 
+fn lex_chunked<'c, 'cc>(chunks: &'c [&'cc [u8]]) -> rxml::Result<usize> {
+	let mut nevents = 0;
+	let mut parser = rxml::FeedParser::new();
+
+	for chunk in chunks {
+		parser.feed(*chunk);
+
+		match parser.read_all(|ev| { nevents += 1 }) {
+			Err(rxml::Error::IO(ioerr)) if ioerr.kind() == io::ErrorKind::WouldBlock => (),
+			Err(e) => return Err(e),
+			Ok(()) => panic!("eof reached before eof"),
+		}
+	}
+
+	parser.feed_eof();
+	parser.read_all(|ev| { nevents += 1})?;
+	Ok(nevents)
+}
+
 fn main() {
     fuzz!(|data: &[u8]| {
-		let mut parser = rxml::FeedParser::new();
-		parser.feed(data.to_vec());
-		parser.feed_eof();
-
-		loop {
-			match parser.read() {
-				Err(_) => return,
-				Ok(None) => return,
-				Ok(Some(_)) => (),
+		let mut had_any_err = false;
+		let mut had_all_err = true;
+		let mut seen_nevents: Option<usize> = None;
+		let mut chunks = Vec::<&[u8]>::with_capacity(2);
+		for i in 0..data.len() {
+			chunks.clear();
+			if i == 0 {
+				chunks.push(data)
+			} else {
+				chunks.push(&data[..i]);
+				chunks.push(&data[i..]);
+			};
+			match lex_chunked(&chunks) {
+				Ok(_) => {
+					had_all_err = false;
+				},
+				Err(_) => {
+					had_any_err = true;
+				},
 			}
 		}
+
     });
 }
