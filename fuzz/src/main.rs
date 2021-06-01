2dbea2c35c38c33b638a0d07d744f412d07d712a
Jonas Schäfer <j.wielicki@sotecware.net>
2021-06-01 22:37:45 +0200

    fuzz: use the NUL byte to indicate chunking positions
    Double-NUL bytes are taken to be a single NUL byte, so that those
    can still be encoded (although they’ll always live at chunk
    boundaries).

--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -12,7 +12,7 @@ fn lex_chunked<'c, 'cc>(chunks: &'c [&'cc [u8]]) -> rxml::Result<usize> {
 	for chunk in chunks {
 		parser.feed(*chunk);
 
-		match parser.read_all(|ev| { nevents += 1 }) {
+		match parser.read_all(|_| { nevents += 1 }) {
 			Err(rxml::Error::IO(ioerr)) if ioerr.kind() == io::ErrorKind::WouldBlock => (),
 			Err(e) => return Err(e),
 			Ok(()) => panic!("eof reached before eof"),
@@ -20,7 +20,7 @@ fn lex_chunked<'c, 'cc>(chunks: &'c [&'cc [u8]]) -> rxml::Result<usize> {
 	}
 
 	parser.feed_eof();
-	parser.read_all(|ev| { nevents += 1})?;
+	parser.read_all(|_| { nevents += 1})?;
 	Ok(nevents)
 }
 
@@ -28,25 +28,35 @@ fn main() {
     fuzz!(|data: &[u8]| {
 		let mut had_any_err = false;
 		let mut had_all_err = true;
-		let mut seen_nevents: Option<usize> = None;
-		let mut chunks = Vec::<&[u8]>::with_capacity(2);
-		for i in 0..data.len() {
-			chunks.clear();
-			if i == 0 {
-				chunks.push(data)
+		let mut chunks = Vec::<&[u8]>::new();
+		let zero = &b"\0"[..];
+		for chunk in data.split(|b| { *b == b'\0' }) {
+			if chunk.len() == 0 {
+				chunks.push(zero)
 			} else {
-				chunks.push(&data[..i]);
-				chunks.push(&data[i..]);
-			};
-			match lex_chunked(&chunks) {
-				Ok(_) => {
-					had_all_err = false;
-				},
-				Err(_) => {
-					had_any_err = true;
-				},
+				chunks.push(chunk)
 			}
 		}
+		match lex_chunked(&chunks) {
+			Ok(_) => {
+				had_all_err = false;
+			},
+			Err(_) => {
+				had_any_err = true;
+			},
+		}
+		let buf = chunks.join(&b""[..]);
+		match lex_chunked(&[&buf]) {
+			Ok(_) => {
+				had_all_err = false;
+			},
+			Err(_) => {
+				had_any_err = true;
+			},
+		}
 
+		if had_any_err && !had_all_err {
+			panic!("error state depends on chunking")
+		}
     });
 }
