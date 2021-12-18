7a3aa82efa3f448cf7355805757550c1d9fa4ddf
Jonas Schäfer <j.wielicki@sotecware.net>
2021-12-18 10:59:42 +0100

    Add pipe example

---
+++ rxml/examples/pipe.rs
@@ -0,0 +1,26 @@
+use std::io;
+use std::io::Write;
+
+use bytes::BytesMut;
+
+use rxml::{PullParser, EventRead, Error};
+use rxml::writer::Encoder;
+
+
+fn main() {
+	let stdin = io::BufReader::new(io::stdin());
+	let mut stdout = io::stdout();
+	let mut enc = Encoder::new();
+	let mut parser = PullParser::new(stdin);
+	let mut buf = BytesMut::with_capacity(8192);
+	let result = parser.read_all(|ev| {
+		enc.encode_event_into_bytes(&ev, &mut buf).expect("failed to encode xml");
+		stdout.write_all(&buf[..]).expect("failed to write to stdout");
+		buf.clear();
+	});
+	match result {
+		Ok(()) => (),
+		Err(Error::IO(e)) => panic!("I/O error: {}", e),
+		Err(e) => panic!("invalid XML on input: {}", e),
+	}
+}
